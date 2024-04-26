502356dace7ab7f6ca08ec671ecfc875cbcdcfec
Solomon <huynhgiahung345345@gmail.com>
2024-04-26 07:28:29 +0700

    fix show icon tool when click and convert fragment

--- app/src/main/java/com/example/moneymanager/HomeFragment.java
+++ app/src/main/java/com/example/moneymanager/HomeFragment.java
@@ -7,11 +7,18 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.core.view.MenuHost;
+import androidx.core.view.MenuProvider;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.Lifecycle;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -24,7 +31,7 @@ import com.example.moneymanager.Model.JarDetail;
  * Use the {@link HomeFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements MenuProvider {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -32,8 +39,7 @@ public class HomeFragment extends Fragment {
     private static final String ARG_PARAM2 = "param2";
 
     // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+
     Context context = getActivity();
     DatabaseHelper db;
 
@@ -70,31 +76,11 @@ public class HomeFragment extends Fragment {
         tvSoDu.setText(String.valueOf(tongThuNhap-tongChiTieu));
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
 
     }
 
@@ -115,12 +101,8 @@ public class HomeFragment extends Fragment {
 
         SharedPreferences prefs = context.getSharedPreferences("getIdUser", MODE_PRIVATE);
         idCurrentLoginUser = prefs.getInt("idUserCurrent", -1);
-        xml_payment.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                replaceFracment(new PersionFragment());
-            }
-        });
+        MenuHost menuHost = requireActivity();
+        menuHost.addMenuProvider(this, getViewLifecycleOwner(), Lifecycle.State.RESUMED);
 
         setEvent();
 
@@ -137,14 +119,12 @@ public class HomeFragment extends Fragment {
     }
 
     private void setEvent() {
-//        frKhoanNo.(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//                    Context context = getActivity();
-//                    Intent intent = new Intent(context, SpendingActivity.class);
-//                    startActivity(intent);
-//                }
-//            });
+       xml_payment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                replaceFracment(new PersionFragment());
+            }
+        });
 
     }
 
@@ -160,5 +140,24 @@ public class HomeFragment extends Fragment {
         tvSoDuHu6 =  rootView.findViewById(R.id.tvSoDuHu6);
         tvSoDu = rootView.findViewById(R.id.tvSoDu);
         xml_payment = rootView.findViewById(R.id.xml_payment);
+    }
+
+    @Override
+    public void onCreateMenu(@NonNull Menu menu, @NonNull MenuInflater menuInflater) {
+        menuInflater.inflate(R.menu.main_top_bar,menu);
+    }
+
+    @Override
+    public boolean onMenuItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId())
+        {
+            case R.id.iconStatis:
+                replaceFracment(new StatiscalFragment());
+                break;
+            case R.id.iconNotify:
+                replaceFracment(new NotifyFragment());
+                break;
+        }
+        return true;
     }
 }
