d7fe90d1c7eb6f1e70bd2d14d96d2a46e816a235
Pham Gia Bao <bao126nvc@gmail.com>
2024-05-04 09:38:15 +0700

    Sua thong tin user

--- app/src/main/java/com/example/personalfinancialmanagement/PersionFragment.java
+++ app/src/main/java/com/example/personalfinancialmanagement/PersionFragment.java
@@ -1,5 +1,8 @@
 package com.example.personalfinancialmanagement;
 
+import static android.content.Context.MODE_PRIVATE;
+
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -15,10 +18,17 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.personalfinancialmanagement.Model.User;
 
 
 public class PersionFragment extends Fragment implements MenuProvider {
-
+    EditText edtFullname, edtEmail, edtUsername;
+    Button btnLuu;
+    int idCurrentLoginUser;
+    DatabaseHelper db;
 
     public PersionFragment() {
         // Required empty public constructor
@@ -40,6 +50,32 @@ public class PersionFragment extends Fragment implements MenuProvider {
         MenuHost menuHost = requireActivity();
         menuHost.addMenuProvider(this, getViewLifecycleOwner(), Lifecycle.State.RESUMED);
 
+        // Set control
+        edtFullname = rootView.findViewById(R.id.edtFullname);
+        edtEmail = rootView.findViewById(R.id.edtEmail);
+        edtUsername = rootView.findViewById(R.id.edtUserName);
+        btnLuu = rootView.findViewById(R.id.btnLuu);
+
+        SharedPreferences prefs = getActivity().getSharedPreferences("getIdUser", MODE_PRIVATE);
+        idCurrentLoginUser = prefs.getInt("idUserCurrent", -1);
+
+        db = new DatabaseHelper(getContext());
+
+        User user = db.getUserByID(idCurrentLoginUser);
+        edtUsername.setText(user.getUsername());
+        edtFullname.setText(user.getFullname());
+        edtEmail.setText(user.getEmail());
+
+        btnLuu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                user.setFullname(edtFullname.getText().toString());
+                user.setEmail(edtEmail.getText().toString());
+                user.setUsername(edtUsername.getText().toString());
+                db.updateUser(user);
+                replaceFracment(new HomeFragment());
+            }
+        });
         return  rootView;
 
     }
